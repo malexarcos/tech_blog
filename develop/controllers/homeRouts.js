9bd9db6ec0be8ebf114cf3d8c60fb47865dde3fd
malexarcos <110743192+malexarcos@users.noreply.github.com>
2023-01-12 23:03:27 -0600

    added code and files

---
+++ develop/controllers/homeRouts.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+router.get('/', (req, res) => {
+    res.render('homepage')
+});
+
+router.get('/login', (req, res) => {
+    res.render('login')
+});
+
+module.exports = router
